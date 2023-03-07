dde7c1a82c5cfd034fa52cd2163cc40596554f26
Selim Fazylov <selim.fazylov1@gmail.com>
2023-03-08 00:38:14 +0200

    add people

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import People from './people';
 function App() {
   return (
     <div className="App">
-      <People/>
+      <People continent='america' />
+      <People continent='europa'/>
+      <People continent='asia'/>
     </div>
   );
 }
--- src/people/People.jsx
+++ src/people/People.jsx
@@ -1,12 +1,21 @@
 import s from './People.module.scss';
 
-function People() {
+function People({continent}) {
+    const onClickPeople = (e) => {
+        console.dir(e.target.id);
+        console.log('continent:', continent)
+        console.log('=============================')
+    }
+
     return (
         <div className={s.main_box}>
-            people
-            <span className={s.first_man}></span>
-            <span className={s.second_man}></span>
-            <span className={s.third_man}></span>
+            <span className={s.first_man} onClick={onClickPeople} id={'first'}>
+                <span className={s.second_man} id={'second'}>
+                    <span className={s.third_man} id={'third'}></span>
+                </span>
+            </span>
+            
+            
         </div>
     )
 };
